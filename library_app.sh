e0e4b640cbd6b54bf040bef721f79d22f578022d
Chalani Dissanayaka <15415@coderacademy.edu.au>
2024-05-05 20:48:12 +1000

    fixed the module not found error and added library_app.sh

---
+++ library_app.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd src/onlineBookLibrary || exit
+python3 main.py
---
+++ src/onlineBookLibrary/book_database/logic.py
@@ -0,0 +1,20 @@
+def calculate_average_rating(total_rating_score, number_of_read_times):
+    average_rating = round((total_rating_score / number_of_read_times), 2)
+    return average_rating
+
+
+def calculate_total_rating_score(total_rating_score, book_rating):
+    total_rating_score = total_rating_score + book_rating
+    return total_rating_score
+
+
+def calculate_number_of_read_times(number_of_read_times):
+    number_of_read_times = number_of_read_times + 1
+    return number_of_read_times
+
+
+def validate_rating_score(book_rating) -> bool:
+    if book_rating < 1 or book_rating > 5:
+        return False
+    else:
+        return True
